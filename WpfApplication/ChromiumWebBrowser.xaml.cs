541eba4f9aa0c893ef6869d3d11dce96f28a329b
long.anh.nguyen <long.anh.nguyen@logigear.com>
2016-12-23 10:27:26 +0700

    first commit

---
+++ WpfApplication/ChromiumWebBrowser.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for ChromiumWebBrowser.xaml
+    /// </summary>
+    public partial class ChromiumWebBrowser : Page
+    {
+        public ChromiumWebBrowser()
+        {
+            CefSettings setting = new CefSettings();
+            setting.RemoteDebuggingPort = 8080;
+            if(!Cef.IsInitialized)
+                Cef.Initialize(setting);
+            InitializeComponent();
+        }
+    }
+}
